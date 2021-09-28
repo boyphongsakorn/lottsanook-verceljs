a44cc32ab534ceb875bdf43b49af01709a9426e1
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-09-28 09:35:41 +0700

    test

--- api/all.js
+++ api/all.js
@@ -582,10 +582,10 @@ router.get('/god', async (req, res) => {
         res.send(yearlist)
     });*/
     fetch('https://raw.githubusercontent.com/boyphongsakorn/testrepo/main/god')
-    .then(res => res.json())
-    .then((body) => {
-        res.send(body)
-    })
+        .then(res => res.json())
+        .then((body) => {
+            res.send(body)
+        })
 })
 
 router.get('/gdpy', async (req, res) => {
@@ -743,28 +743,36 @@ router.get('/finddol', async (req, res) => {
     let channels
     let allwin = []
     if (req.query.search.length > 3) {
-    await fetch('https://lottsanook-verceljs.vercel.app/api/god')
-        .then(res => res.json())
-        .then((body) => {
-            channels = body.splice(408)
-            console.log(channels)
-        })
-    for (const val of channels) {
-        console.log(val)
-        await fetch('https://lottsanook-verceljs.vercel.app/api/?date=' + val + '&from')
+        /*await fetch('https://lottsanook-verceljs.vercel.app/api/god')
             .then(res => res.json())
             .then((body) => {
-                for (let index = 0; index < body.length; index++) {
-                    const element = body[index];
-                    if (element.includes(req.query.search.toString())) {
-                        allwin.push(body[0][0])
-                        console.log('https://lottsanook-verceljs.vercel.app/api/?date=' + val + '&from')
-                    }
-                }
-
+                channels = body.splice(408)
+                console.log(channels)
             })
-    }
-    res.send(allwin)
+        for (const val of channels) {
+            console.log(val)
+            await fetch('https://lottsanook-verceljs.vercel.app/api/?date=' + val + '&from')
+                .then(res => res.json())
+                .then((body) => {
+                    for (let index = 0; index < body.length; index++) {
+                        const element = body[index];
+                        if (element.includes(req.query.search.toString())) {
+                            allwin.push(body[0][0])
+                            console.log('https://lottsanook-verceljs.vercel.app/api/?date=' + val + '&from')
+                        }
+                    }
+
+                })
+        }
+        res.send(allwin)*/
+        await fetch('https://raw.githubusercontent.com/boyphongsakorn/testrepo/main/tmp/' + req.query.search.toString())
+            .then(res => res.json())
+            .then((body) => {
+                res.send(body)
+            }).catch((err) => {
+                res.send(allwin)
+                console.log(err)
+            });
     } else {
         fetch('https://astro.meemodel.com/%E0%B8%A7%E0%B8%B4%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%B0%E0%B8%AB%E0%B9%8C%E0%B9%80%E0%B8%A5%E0%B8%82%E0%B8%AB%E0%B8%A7%E0%B8%A2/' + req.query.search, { redirect: 'error' })
             .then(res => res.text())
@@ -772,10 +780,10 @@ router.get('/finddol', async (req, res) => {
                 let $ = cheerio.load(body)
                 $('td').toArray().forEach(element => {
                     let sl = element.firstChild.data
-                    if(sl != null && sl.split(" ").length == 3 && sl.split(" ")[2] >= 2550){
+                    if (sl != null && sl.split(" ").length == 3 && sl.split(" ")[2] >= 2550) {
                         allwin.unshift(sl)
                     }
-                    
+
                 });
                 res.send(allwin)
             });
