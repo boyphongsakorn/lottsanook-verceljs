544916568434509850cb9907019faebe43f9ca1e
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2022-06-06 19:55:59 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+//const product = require("./api/product");
+//const index = require("./api/index");
+//const index2 = require("./api/index2");
+const all = require("./api/all");
+
+process.env.TZ = 'Asia/Bangkok'
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/", all);
+app.use("/", all);
+/*app.use("/api/", index);
+app.use("/api/index2", index2);
+app.use("/api/product", product);*/
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
